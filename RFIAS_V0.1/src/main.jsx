fcac0fa796c3915f54f3f0ab7acdc9244ee112ab
C.N.K.Dissanayake <nisaka2333@gmail.com>
2024-02-10 11:19:03 +0530

    log in page front end completed

--- RFIAS_V0.1/src/main.jsx
+++ RFIAS_V0.1/src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import Login from "./components/LoginForm.jsx";
+import Signup from "./components/signup.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Login />
+    <Signup/>
   </React.StrictMode>
 );
