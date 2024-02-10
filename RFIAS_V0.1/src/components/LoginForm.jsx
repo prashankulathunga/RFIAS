54f48160a18bfd629bd7ca96f7840dd63ec8ec74
C.N.K.Dissanayake <nisaka2333@gmail.com>
2024-02-10 11:19:03 +0530

    log in page front end completed

--- RFIAS_V0.1/src/components/LoginForm.jsx
+++ RFIAS_V0.1/src/components/LoginForm.jsx
@@ -1,36 +1,47 @@
-
-import './LoginForm.css';
+import "./LoginForm.css";
 
 const LoginForm = () => {
+  return (
+    <div className="wrapper">
+      <div class="screen">
+        <div class="screen__content">
+          <form action="">
+            <div className="inputbox">
+              <input typeof="text" placeholder="Username" required />
+            </div>
+            <div className="inputbox">
+              <input typeof="password" placeholder="Password" required />
+            </div>
+            <div className="remember_me">
+              <input type="checkbox"  id="rememberMe" />
+              <label for='rememberMe'>Remember me </label>
+            </div>
+            <div className="forgotpassword?">
+              <a  className="forgotpassword" href="#">Forgot password?</a>
+            </div>
 
-    return (
-        // <div className = 'wrapper'> 
-        // <form action=''>
-        //     <h1> Login</h1>
-        //     <div className= "input-box">
-        //         <input type='text' placeholder='Username' required></input>
-        //     </div>
-        //     <div className= "input-box">
-        //         <input type='password' placeholder='Password' required></input>
-        //     </div>
-        //     <div className='remember-forget'>
-        //         <label> <input type= 'checkbox'> Remember me </input></label>
-        //         <a href='#'>Forget password</a>
-        //     </div>
+            <button type="submit" class="button" role="button">
+              {" "}
+              Login{" "}
+              <i class="button__icon fas fa-chevron-right"></i>
+            </button>
+            <div className="signup">
+              <p>
+                Dont have an account? <a className="register" href="#">Register</a>
+              </p>
+            </div>
+          </form>
+        </div>
 
-        //     <button type="submit"> Login</button>
-            
-        //     <div className="signup">
-        //         <p>Dont have an account? <a href='#'>Register</a></p>
-        //     </div>
-        // </form>
-
-         
-        // </div>
-
-        <h1>Hello</h1>
-    );
-}
+        <div class="screen__background">
+          <span class="screen__background__shape screen__background__shape4"></span>
+          <span class="screen__background__shape screen__background__shape3"></span>
+          <span class="screen__background__shape screen__background__shape2"></span>
+          <span class="screen__background__shape screen__background__shape1"></span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default LoginForm;
-
