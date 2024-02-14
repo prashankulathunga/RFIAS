73bdd3218e5542a7cd516f87f406d2fed0708e81
C.N.K.Dissanayake <nisaka2333@gmail.com>
2024-02-14 13:13:15 +0530

    research css

--- RFIAS_V0.1/src/components/user/research.jsx
+++ RFIAS_V0.1/src/components/user/research.jsx
@@ -1,54 +1,101 @@
 import { Button, FormLabel, Table } from "react-bootstrap";
 import { useRef, useState } from "react";
+import "./research.css";
 
 function Research() {
-    const [files, setFiles] = useState(null);
-    const inputRef = useRef();
-  
-    const handleDragOver = (event) => {
-      event.preventDefault();
-    };
-    const handleDrop = (event) => {
-      event.preventDefault();
-      setFiles(Array.from(event.dataTransfer.files));
-    };
-  
-    const handleUpload = () => {};
-  
-    return (
-      <div className="page">
-        <div className="filesubmit">
-          <h1>Research submit report</h1>
-          {files ? (
-            <div className="uploads">
-              <ul>
-                {files.map((file, idx) => (
-                  <li key={idx}>{file.name}</li>
-                ))}
-              </ul>
-              <div className="actions">
-                <button onClick={() => setFiles(null)}>Cancel</button>
-                <button onClick={handleUpload}>Upload</button>
-              </div>
-            </div>
-          ) : (
-            <div className="dropzone" onDragOver={handleDragOver} onDrop={handleDrop}>
-              <p>Drag and drop files here</p>
-              <input
-                type="file"
-                multiple
-                onChange={(event) => setFiles(Array.from(event.target.files))}
-                hidden
-                ref={inputRef}
-              />
-              <Button onClick={() => inputRef.current.click()}>Select Files</Button>
-            </div>
-          )}
+  const [files, setFiles] = useState(null);
+  const inputRef = useRef();
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+  const handleDrop = (event) => {
+    event.preventDefault();
+    setFiles(Array.from(event.dataTransfer.files));
+  };
+
+  const handleUpload = () => {};
+
+  return (
+    <div className="page">
+      <div className="stas">
+        <div className="box">
+          <i className="uli-file">
+            <h3 id="statmaintext"> Complete</h3>
+            <p id="statbottomtext">05</p>
+          </i>
         </div>
+        <div className="box">
+          <i className="uli-file">
+            <h3 id="statmaintext"> Running</h3>
+            <p id="statbottomtext">02</p>
+          </i>
+        </div>
+        <div className="box">
+          <i className="uli-file">
+            <h3 id="statmaintext"> Total Research </h3>
+            <p id="statbottomtext">05</p>
+          </i>
+        </div>
+        <div className="box">
+          <i className="uli-file">
+            <h3 id="statmaintext"> Hold</h3>
+            <p id="statbottomtext">05</p>
+          </i>
+        </div>
+      </div>
+      <br />
+
+      <div className="filesubmit">
+        <br />
+        <h3>Research submit report</h3>
+        <br />
+        
+
+        {files ? (
+          <div className="uploads">
+            <ul>
+              {files.map((file, idx) => (
+                <li key={idx}>{file.name}</li>
+              ))}
+            </ul>
+            <div className="actions">
+              <button onClick={() => setFiles(null)} className="selectcancel">
+                Cancel
+              </button>{" "}
+              {""}
+              <button onClick={handleUpload} className="selectupload">
+                Submit
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div
+            className="dropzone"
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+          >
+            <p>Drag and drop files here</p>
+            <input
+              className="inputdropzone"
+              type="file"
+              multiple
+              onChange={(event) => setFiles(Array.from(event.target.files))}
+              hidden
+              ref={inputRef}
+            />
+            <Button onClick={() => inputRef.current.click()}>
+              Select Files
+            </Button>
+          </div>
+        )}
+      </div>
+      <br/>
+      <br/>
+      <br/>
       <div className="researchoverview">
         <h1>Research overview</h1>
 
-        
         <br></br>
         <Table responsive>
           <thead>
