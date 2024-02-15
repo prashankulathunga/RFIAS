40e21cde88a5cf3511b03f792ee88294bf7dab60
C.N.K.Dissanayake <nisaka2333@gmail.com>
2024-02-15 10:09:19 +0530

    j

--- RFIAS_V0.1/src/main.jsx
+++ RFIAS_V0.1/src/main.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import Login from "./components/login/LoginForm.jsx";
 import Signup from "./components/login/signup.jsx";
 import Leave from "./components/user/userleave.jsx";
 import Research from "./components/user/research.jsx";
 import Sidebar from "./components/user/sidebar.jsx";
 import Udashboard from "./components/user/userdashboard.jsx";
 import Structure from "./components/user/structure.jsx";
+import LoginForm from "./components/login/LoginForm.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Structure/>
+    <App />
   </React.StrictMode>
 );
